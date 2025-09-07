620f20afc73f0e042d78b2d5171c0a23afaf45bc
hemreozalp <h.emreozalpp@gmail.com>
2025-09-08 00:15:37 +0300

    refactor(notification): add logging for testing

--- notification-service/src/main/java/com/ecommerce/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/ecommerce/notification_service/service/NotificationService.java
@@ -1,13 +1,15 @@
 package com.ecommerce.notification_service.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class NotificationService {
 
     @KafkaListener(topics = "order-events", groupId = "notification-group")
     public void handleOrderEvent(String message) {
-        System.out.println("Notification received: " + message);
+        log.info("📥 Received OrderCreatedEvent: {}", message);
     }
 }
